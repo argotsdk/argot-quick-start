09d3349b365d6f79c253a143609bed978cdb783c
Dan Midwood <dan@danmidwood.com>
2014-01-14 14:46:52 -0500

    Add separate tests for reading and writing

--- src/test/java/com/argot/start/TestWritingAndReading.java
+++ src/test/java/com/argot/start/TestWritingAndReading.java
@@ -1,9 +1,10 @@
 package com.argot.start;
 
-import com.argot.*;
+import com.argot.TypeException;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,12 +12,35 @@ public class TestWritingAndReading {
 
     @Test
     public void readDataIsEqualToWhatWasWritten() throws TypeException, IOException {
-        Colour input = new Colour((short)120,(short)10,(short)30);
+        Colour input = new Colour((short) 120, (short) 10, (short) 30);
         StartHere subject = new StartHere();
         byte[] data = subject.writeColour(input);
+        System.out.println(Arrays.toString(data));
         Colour output = subject.readColour(data);
         assertEquals(input.red, output.red);
         assertEquals(input.green, output.green);
         assertEquals(input.blue, output.blue);
+    }
+
+    @Test
+    public void writingAColorGivesThreeByteArray() throws TypeException, IOException {
+        Colour input = new Colour((short) 120, (short) 10, (short) 30);
+        StartHere subject = new StartHere();
+        byte[] data = subject.writeColour(input);
+
+        assertEquals(input.red, data[0]);
+        assertEquals(input.green, data[1]);
+        assertEquals(input.blue, data[2]);
+    }
+
+    @Test
+    public void readingAThreeByteArrayGivesColour() throws TypeException, IOException {
+        StartHere subject = new StartHere();
+        byte[] data = new byte[]{120, 10, 30};
+        System.out.println(Arrays.toString(data));
+        Colour output = subject.readColour(data);
+        assertEquals(120, output.red);
+        assertEquals(10, output.green);
+        assertEquals(30, output.blue);
     }
 }
